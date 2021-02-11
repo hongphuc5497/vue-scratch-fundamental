96909965fff347a763fb750e657963a1fecfb12a
Hong Phuc <hongphuc.dthp@gmail.com>
2021-02-11 13:11:15 +0700

    data render with API fetching

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		products: ['Boots', 'Jacket', 'Pant', 'Shirt'],
+		products: [],
+	},
+	created() {
+		fetch('https://6024c8e236244d001797af40.mockapi.io/products')
+			.then((response) => response.json())
+			.then((json) => {
+				this.products = json;
+			});
 	},
 });
