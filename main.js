ccbf4463b20e59d53ac996f20893deb77bf6a4c0
Hong Phuc <hongphuc.dthp@gmail.com>
2021-02-11 13:58:58 +0700

    Lesson 1 - Vue Instance

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-const app = new Vue({
+var app = new Vue({
 	el: '#app',
 	data: {
-		products: [],
-	},
-	computed: {
-		totalProducts() {
-			return this.products.reduce((sum, product) => sum + product.quantity, 0);
-		},
-	},
-	created() {
-		fetch('https://6024c8e236244d001797af40.mockapi.io/products')
-			.then((response) => response.json())
-			.then((json) => {
-				this.products = json;
-			});
+		product: {
+      name: 'Boot',
+      description: 'This is a boot'
+    },
 	},
 });
+
+// Vue Instance
+
+// {{ }} - Vue Expression
+
+// Reactive
